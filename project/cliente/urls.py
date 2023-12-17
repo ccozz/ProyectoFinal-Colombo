22750d31eedf1b1f1255900f5a3857a85430c3df
ccozz <ccozz@hotmail.com>
2023-12-17 11:21:00 -0300

    busqueda y form added

--- project/cliente/urls.py
+++ project/cliente/urls.py
@@ -5,4 +5,6 @@ app_name = "cliente"
 
 urlpatterns = [
     path('', views.home, name="index"),
+    path('busqueda/', views.busqueda),
+    path('crear/', views.crear),
 ]
--- project/cliente/views.py
+++ project/cliente/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . import models
 
 def home(request):
@@ -8,3 +8,22 @@ def home(request):
     # creo un diccionario para pasar como parámetro en el render donde le explico que voy a llamar a los objetos que traigo con la llamada a la DB
     context = {"clientes": clientes}
     return render(request, "cliente/index.html", context)
+
+def busqueda(request):
+    cliente_nombre = models.Cliente.objects.filter(nombre__contains="John")
+    context = {
+        "cliente_nombre": cliente_nombre
+    }
+    return render(request, "cliente/busqueda.html", context)
+
+from . import forms
+
+def crear(request):
+    if request.method == "POST":
+        form = forms.ClienteForm(request.POST)
+        if form.is_valid():
+            form.save
+            return redirect("cliente:index")
+    else:
+        form = forms.ClienteForm
+    return render(request, "cliente/crear.html", {"form": form})
