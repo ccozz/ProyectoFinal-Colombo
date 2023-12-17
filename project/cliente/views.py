b7aafd4f4c22b514607ecc3f258f90f969a4ed6f
ccozz <ccozz@hotmail.com>
2023-12-17 10:42:11 -0300

    llamada a DB cliente

--- project/cliente/views.py
+++ project/cliente/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from . import models
 
 def home(request):
-    return render(request, "cliente/index.html")
+    # llamada a la base de datos
+    # instancio y llamo objetos
+    clientes = models.Cliente.objects.all()
+    # creo un diccionario para pasar como parámetro en el render donde le explico que voy a llamar a los objetos que traigo con la llamada a la DB
+    context = {"clientes": clientes}
+    return render(request, "cliente/index.html", context)
