acd36cf960c56ee397b2933ac53322f2f4935897
ccozz <ccozz@hotmail.com>
2023-12-23 10:01:14 -0300

    edit producto added

--- project/producto/urls.py
+++ project/producto/urls.py
@@ -6,5 +6,8 @@ app_name = "producto"
 urlpatterns = [
     path('', views.home, name="index"),
     path('busqueda/', views.busqueda, name="productos-buscar"),
+    path('ver_productos', views.ver_productos, name="ver_productos"),
     path('crear/', views.crear, name="crear"),
+    path('eliminar/<int:id>', views.eliminar, name="eliminar"),
+    path('editar/<int:id>', views.editar, name="editar"),
 ]
--- project/producto/views.py
+++ project/producto/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from . import models
 from .forms import ProductoBuscarFormulario
+from . import forms
 
 def home(request):
     """ return render(request, "producto/index.html") """
@@ -29,8 +30,10 @@ def busqueda(request):
             # Manejar el caso cuando el formulario no es válido
             return render(request, "producto/busqueda.html", context={"form": formulario})
 
-
-from . import forms
+def ver_productos(request):
+    productos = models.Producto.objects.all()
+    context = {"productos": productos}
+    return render(request, "producto/productos_list.html", context)
 
 def crear(request):
     if request.method == "POST":
@@ -41,3 +44,28 @@ def crear(request):
     else:
         form = forms.ProductoForm()
     return render(request, "producto/crear.html", {"form": form})
+
+
+def eliminar(request, id):
+    if request.method == "POST":
+        producto = models.Producto.objects.get(id=id)
+        producto.delete();
+
+        productos = models.Producto.objects.all()
+        return render(request, "producto/productos_list.html", {"productos": productos})
+
+def editar(request, id):
+    producto = models.Producto.objects.get(id=id)
+
+    if request.method == "POST":
+        form = forms.ProductoForm(request.POST, instance=producto)
+        
+        if form.is_valid():
+            form.save()
+            return redirect("producto:index")
+        else:
+            return render(request, "producto/editar.html", {"form": form, "id": producto.id})
+        
+    else:
+        form = forms.ProductoForm(instance=producto)
+        return render(request, "producto/editar.html", {"form": form, "id": producto.id})
